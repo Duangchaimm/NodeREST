00478acbcbbd333a8ea4f9e19f7e5906a87e9409
Your Name <you@example.com>
2023-02-18 01:10:06 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 
+
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.send("I'am atomic");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port} `);
 });
